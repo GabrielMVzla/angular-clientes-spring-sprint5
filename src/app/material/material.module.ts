dc16c250aa05c873a293d8c549e569b5d3b1730b
GabrielMVzla <gabriel_montesvzla@hotmail.com>
2021-09-01 12:24:11 -0600

    Update material.module.ts

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -7,12 +7,6 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {MatToolbarModule} from '@angular/material/toolbar';
 
-
-
-
-
-
-
 @NgModule({
   declarations: [],
   imports: [
